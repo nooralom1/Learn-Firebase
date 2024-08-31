5cef42cf5c7b9229553719b6dafa1f6879fffbf8
NS NooR <non377938@gmail.com>
2024-08-31 22:53:31 +0600

    firebase

--- lib/view/common_widget/common_textfield.dart
+++ lib/view/common_widget/common_textfield.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class CommonTextField extends StatelessWidget {
   const CommonTextField({
-    super.key, required this.hintText, required this.controller,
+    super.key,
+    required this.hintText,
+    required this.controller,
   });
   final String hintText;
   final TextEditingController controller;
@@ -15,15 +17,22 @@ class CommonTextField extends StatelessWidget {
         child: TextFormField(
           controller: controller,
           decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              errorBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.red),borderRadius: BorderRadius.circular(10)),
-              focusedErrorBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.red),borderRadius: BorderRadius.circular(10)),
-              hintText: hintText,
+            enabledBorder:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            focusedBorder:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            errorBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.circular(10)),
+            focusedErrorBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.circular(10)),
+            hintText: hintText,
           ),
-          validator: (value){
-            if(value==null || value==""){
-              const ScaffoldMessenger(child: SnackBar(content: Text("This field can't be emty !")));
+          validator: (value) {
+            if (value == null || value == "") {
+              const ScaffoldMessenger(
+                  child: SnackBar(content: Text("This field can't be emty !")));
             }
             return null;
           },

bceb5aa8a846883585126b869dfa3b0ee9d44874
NS NooR <non377938@gmail.com>
2024-09-01 21:33:28 +0600

    firebase

--- lib/controller/firebase_controller/firebase_options.dart
+++ lib/controller/firebase_controller/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
+
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {

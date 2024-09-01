6b2f40d960ef0a1308859d9641fa3298fe61adba
NS NooR <non377938@gmail.com>
2024-09-01 21:33:15 +0600

    firebase

--- lib/controller/firebase_controller/get_info.dart
+++ lib/controller/firebase_controller/get_info.dart
@@ -2,16 +2,15 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
-
-class GetInfoService{
-
-  static Future<QuerySnapshot<Map<String, dynamic>>?> getInfoService()async{
-    try{
+class GetInfoService {
+  static Future<QuerySnapshot<Map<String, dynamic>>?> getInfoService() async {
+    try {
       log("+++++++++++++");
       FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-      QuerySnapshot<Map<String, dynamic>> data = await firebaseFirestore.collection("User Info").get();
+      QuerySnapshot<Map<String, dynamic>> data =
+          await firebaseFirestore.collection("User Info").get();
       return data;
-    }catch(e){
+    } catch (e) {
       log("+++++++22222222222++++++");
       debugPrint("Error: $e");
     }
